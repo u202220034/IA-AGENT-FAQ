fb1da937febfdbdec9adabc2ecd6d2ab740b1c8c
u202220034 <142704058+u202220034@users.noreply.github.com>
2026-01-06 17:18:02 -0500

    Delete tests/test.py

--- tests/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-paylod = {'user_request': 'Who is the boss at SAP?'}
-url = 'https://faq.cfapps.us10-001.hana.ondemand.com'
-headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
-r = requests.get(url, json=paylod, headers=headers, verify=False)
-response = r.json()
-faq_response = response['faq_response'] # final answer
-faq_response_log = response['faq_response_log'] # log of the individual steps
